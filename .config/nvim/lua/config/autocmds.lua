7f61cf21bd4147184f7675b6ebd91c2ca6936aea
Tolemariam Fufa Teso <tolemariamfufat@gmail.com>
2023-09-01 09:58:30 +0300

    nvim deleted

--- .config/nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
---  _____     _      __  __            _
--- |_   _|__ | | ___|  \/  | __ _ _ __(_) __ _ _ __ ___
---   | |/ _ \| |/ _ \ |\/| |/ _` | '__| |/ _` | '_ ` _ \
---   | | (_) | |  __/ |  | | (_| | |  | | (_| | | | | | |
---   |_|\___/|_|\___|_|  |_|\__,_|_|  |_|\__,_|_| |_| |_|
-
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
